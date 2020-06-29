0506f14ec7c6d1575cb488decf6e5a6ce0975591
FBehrad <41340554+FBehrad@users.noreply.github.com>
2020-06-29 23:17:31 +0430

    Update model.py

--- model.py
+++ model.py
@@ -470,8 +470,7 @@ def build_model(input_shape=(4, 160, 192, 128), output_channels=3, weight_L2=0.1
     model.compile(
         adam(lr=1e-4),
         [loss_gt(dice_e), loss_VAE(input_shape, z_mean, z_var, weight_L2=weight_L2, weight_KL=weight_KL)],
-        metrics=[metrics.Accuracy(),
-                 dice_coefficient]
+        metrics=[dice_coefficient]
     )
 
     return model
